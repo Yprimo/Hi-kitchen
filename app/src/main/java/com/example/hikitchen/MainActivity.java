289b0fa7e67f83ecce59358b80de59eecd716c5b
YK <1090949071@qq.com>
2018-12-03 00:52:23 +0800

    第一次commit，实现底部导航栏

--- app/src/main/java/com/example/hikitchen/MainActivity.java
+++ app/src/main/java/com/example/hikitchen/MainActivity.java
@@ -10,10 +10,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.support.v4.app.FragmentTransaction;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
@@ -31,7 +28,6 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
