60db5ee673b2f1290066211dd9d3da68232c4dde
YK <1090949071@qq.com>
2018-12-03 13:20:06 +0800

    第3次commit

---
+++ app/src/main/java/com/example/hikitchen/MyFragment.java
@@ -0,0 +1,42 @@
+package com.example.hikitchen;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MyFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MyFragment extends Fragment {
+
+   private String content;
+    public static MyFragment newInstance(String content) {
+        MyFragment newFragment = new MyFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("content", content);
+        newFragment.setArguments(bundle);
+        return newFragment;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1_content,container,false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            content= args.getString("content");
+        }
+    }
+}
