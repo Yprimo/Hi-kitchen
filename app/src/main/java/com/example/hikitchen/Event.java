d780780ede6221dab074879d3b76dd836950c86d
yk <1090949071@qq.com>
2019-05-14 23:45:18 +0800

    实现循环播报

---
+++ app/src/main/java/com/example/hikitchen/Event.java
@@ -0,0 +1,16 @@
+package com.example.hikitchen;
+
+/**
+ * Created by Administrator on 2019/5/12.
+ */
+public class Event {
+
+    private String mMsg;
+    public Event(String msg) {
+        // TODO Auto-generated constructor stub
+        mMsg = msg;
+    }
+    public String getMsg(){
+        return mMsg;
+    }
+}
